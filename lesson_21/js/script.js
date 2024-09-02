a653ba6d11b32ebc3ec112de7c166d5b43eac533
ostinska <162920321+ostinska@users.noreply.github.com>
2024-09-02 15:23:13 +0300

    Add files via upload

---
+++ lesson_21/js/script.js
@@ -0,0 +1,42 @@
+/*let userNeme = "Sasha"
+
+console.log(userNeme)
+
+let userAge = 23
+console.log(userAge)
+// зміна значень
+userAge = 32
+console.log(userAge)
+
+userNeme = "Masha"
+console.log(userNeme)
+
+// копіюівння значення
+let otherUserNeme
+userNeme = "Dasha"
+console.log(otherUserNeme)
+otherUserNeme = userNeme
+console.log(otherUserNeme)
+
+// видимість
+userNeme = 25
+console.log(userAge)
+function someFunction() {//block
+	userAge = 13
+	console.log(userAge)
+
+}
+someFunction()
+
+
+const userName = "Nataliii"
+console.log(userName)
+
+
+// типи даних
+
+let numOne = 20
+let numTwo = "20"
+let numSumm = numOne + numTwo
+console.log(numSumm)
+*/
