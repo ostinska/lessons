6b13342369023373dd2319cdaf24c81e1711e00b
ostinska <162920321+ostinska@users.noreply.github.com>
2024-10-03 17:04:59 +0300

    Add files via upload

---
+++ lesson_22/js/script.js
@@ -0,0 +1,39 @@
+/*
+let someVar = 0;
+++someVar;
+
+if (someVar) {
+	console.log(someVar);
+}
+
+for (let i = 1; i <= 10; i++) { console.log(`Пункт ${i}`) }
+
+let result = 2 * 20 <= 10 || 30 / 2 < 5 && 10 <= "10" || 20 === "20"
+console.log(typeof result);
+console.log(result);
+
+
+
+
+let masuvDz = ["HI", "5", 10, false, "Саша"]
+
+masuvDz.forEach(masuvDz => {
+	if (masuvDz === 10) {
+		console.log(masuvDz);
+	}
+
+})
+	
+function calcDZ(a, b) {
+	const result = a / b
+	{ return `Результат ділення: ${result}` }
+}
+*/
+
+
+
+
+
+
+
+
